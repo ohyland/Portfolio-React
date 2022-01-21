ba2baa030327eb23135fc199f1ea6d937dd683bc
Olivia Hyland <1oliviahyland@gmail.com>
2022-01-21 21:42:25 +0000

    Implement Grid to layout work on portfolio page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,31 +3,36 @@ import todoListPhoto from '../images/todo-list.jpg';
 import weatherAppPhoto from '../images/weather-app.jpg';
 import twitterPhoto from '../images/twitter.jpg';
 import gamesAppPhoto from '../images/games.jpg';
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Grid,
+    IconButton,
+    Tooltip,
+} from '@mui/material';
 import { makeStyles } from '@mui/styles';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import {
     LaptopMac as LaptopMacIcon,
     GitHub as GitHubIcon,
 } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
 
 const useStyles = makeStyles({
-    icons: {
-        display: 'flex',
-        '& .MuiButtonBase-root': {
-            backgroundColor: 'white',
+    portfolio: {
+        '& .MuiGrid-root': {
+            justifyContent: 'space-between',
         },
-    },
-    works: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        '& .MuiPaper-root': {
-            margin: '5px',
-            '& .MuiTypography-root': {
-                color: 'white',
+        // '& .MuiPaper-root': {
+        //     maxWidth: '85%',
+        // },
+        '& .MuiTypography-root': {
+            color: 'white',
+        },
+        '& .icons': {
+            display: 'flex',
+            '& .MuiButtonBase-root': {
+                backgroundColor: 'white',
             },
         },
     },
@@ -36,114 +41,170 @@ const useStyles = makeStyles({
 const Portfolio = () => {
     const classes = useStyles();
     return (
-        <>
-            <Typography variant="h4">Things I've built...</Typography>
-            <div className={classes.works}>
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={todoListPhoto}
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Todo List
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            It was popularised in the 1960s with the release of
-                            Letraset sheets containing Lorem Ipsum passages
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <IconButton color="secondary">
-                            <GitHubIcon />
-                        </IconButton>
-                        <IconButton color="secondary">
-                            <LaptopMacIcon />
-                        </IconButton>
-                    </CardActions>
-                </Card>
+        <div className={classes.portfolio}>
+            <Typography variant="h4" component="h1">
+                Things I've built...
+            </Typography>
+            <Grid container>
+                <Grid item xs={12} sm={5} md={4} lg={3}>
+                    <Card variant="outlined" sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={todoListPhoto}
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="h2"
+                            >
+                                Todo List
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Todo List App created with React, Redux,
+                                Typescript and JSS
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Tooltip title="View code">
+                                <IconButton
+                                    color="secondary"
+                                    href="https://github.com/ohyland/Todo-List-React"
+                                >
+                                    <GitHubIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="View live">
+                                <IconButton
+                                    href="https://reacttodolist20.netlify.app/"
+                                    color="secondary"
+                                >
+                                    <LaptopMacIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={5} md={4} lg={3}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={weatherAppPhoto}
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="h2"
+                            >
+                                Weather App
+                            </Typography>
 
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={weatherAppPhoto}
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Weather App
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            It was popularised in the 1960s with the release of
-                            Letraset sheets containing Lorem Ipsum passages
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <IconButton color="secondary">
-                            <GitHubIcon />
-                        </IconButton>
-                        <IconButton color="secondary">
-                            <LaptopMacIcon />
-                        </IconButton>
-                    </CardActions>
-                </Card>
+                            <Typography variant="body2" color="text.secondary">
+                                Weather app created using React and weather data
+                                fetched using Axios
+                            </Typography>
+                        </CardContent>
 
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={twitterPhoto}
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Twitter clone
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            It was popularised in the 1960s with the release of
-                            Letraset sheets containing Lorem Ipsum passages
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <IconButton color="secondary">
-                            <GitHubIcon />
-                        </IconButton>
-                        <IconButton color="secondary">
-                            <LaptopMacIcon />
-                        </IconButton>
-                    </CardActions>
-                </Card>
-
-                <Card sx={{ maxWidth: 345 }}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={gamesAppPhoto}
-                        alt="green iguana"
-                    />
-                    <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                            Games App
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            It was popularised in the 1960s with the release of
-                            Letraset sheets containing Lorem Ipsum passages
-                        </Typography>
-                    </CardContent>
-                    <CardActions>
-                        <IconButton color="secondary">
-                            <GitHubIcon />
-                        </IconButton>
-                        <IconButton color="secondary">
-                            <LaptopMacIcon />
-                        </IconButton>
-                    </CardActions>
-                </Card>
-            </div>
-        </>
+                        <CardActions>
+                            <Tooltip title="View code">
+                                <IconButton
+                                    href="https://github.com/ohyland/weather"
+                                    color="secondary"
+                                >
+                                    <GitHubIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="View live">
+                                <IconButton color="secondary">
+                                    <LaptopMacIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={5} md={4} lg={3}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={twitterPhoto}
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="h2"
+                            >
+                                Twitter clone
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Twitter Clone created using React and styled
+                                using Material UI
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Tooltip title="View code">
+                                <IconButton
+                                    color="secondary"
+                                    href="https://github.com/ohyland/twitter-clone"
+                                >
+                                    <GitHubIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="View live">
+                                <IconButton color="secondary">
+                                    <LaptopMacIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </CardActions>
+                    </Card>
+                </Grid>
+                <Grid item xs={12} sm={5} md={4} lg={3}>
+                    <Card sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={gamesAppPhoto}
+                            alt="green iguana"
+                        />
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="h2"
+                            >
+                                Games App
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Games App created using React, react-router,
+                                Redux and fetching data from an API
+                            </Typography>
+                        </CardContent>
+                        <CardActions>
+                            <Tooltip title="View code">
+                                <IconButton
+                                    href="https://github.com/ohyland/ignite"
+                                    color="secondary"
+                                >
+                                    <GitHubIcon />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="View live">
+                                <IconButton color="secondary">
+                                    <LaptopMacIcon />
+                                </IconButton>
+                            </Tooltip>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Grid>
+        </div>
     );
 };
 
