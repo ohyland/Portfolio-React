35181e7665785a2fb2c1ede119b187d2ab36e5a6
Olivia Hyland <1oliviahyland@gmail.com>
2022-01-21 21:37:19 +0000

    create contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+import { Button, TextField, Typography, Alert, Snackbar } from '@mui/material';
 import { makeStyles } from '@mui/styles';
+import axios from 'axios';
 
 const useStyles = makeStyles({
     form: {
-        width: '50%',
         '& input': {
             color: 'white',
             fontStyle: 'italic',
@@ -17,17 +17,42 @@ const useStyles = makeStyles({
 });
 
 const Contact = () => {
+    const formId = 'PFUsPpdF';
+    const formSparkUrl = `https://submit-form.com/${formId}`;
     const classes = useStyles();
-    const [values, setValue] = useState({
+    const [formValues, setFormValues] = useState({
         name: '',
         email: '',
         message: '',
     });
+    const [submitting, setSubmitting] = useState(false);
+    const [message, setMessage] = useState();
+    const [open, setOpen] = useState(false);
 
     const onSubmitForm = (event) => {
         event.preventDefault();
-        const { name, email, message } = values;
-        console.log('Name: ', name, 'Email: ', email, 'Message: ', message);
+        const { name, email, message } = formValues;
+        setFormValues({ Name: name, Email: email, Message: message });
+        setSubmitting(true);
+        postSubmission();
+        setSubmitting(false);
+        setFormValues({});
+    };
+
+    const postSubmission = async () => {
+        const payload = formValues;
+        try {
+            const result = await axios.post(formSparkUrl, payload);
+            console.log('result: ', result);
+            setOpen(true);
+            setMessage('Thank you, Your message has been sent!');
+        } catch (error) {
+            setMessage('Sorry, there was a problem, please try again');
+        }
+    };
+
+    const handleClose = () => {
+        setOpen(false);
     };
 
     return (
@@ -37,10 +62,13 @@ const Contact = () => {
                 <div>
                     <TextField
                         fullWidth
-                        placeholder="Firstname"
+                        placeholder="Name"
                         required
                         onChange={(event) =>
-                            setValue({ ...values, name: event.target.value })
+                            setFormValues({
+                                ...formValues,
+                                name: event.target.value,
+                            })
                         }
                     ></TextField>
                 </div>
@@ -51,7 +79,10 @@ const Contact = () => {
                         placeholder="Email"
                         required
                         onChange={(event) =>
-                            setValue({ ...values, email: event.target.value })
+                            setFormValues({
+                                ...formValues,
+                                email: event.target.value,
+                            })
                         }
                     ></TextField>
                 </div>
@@ -61,18 +92,41 @@ const Contact = () => {
                         required
                         placeholder="Type your message here..."
                         onChange={(event) =>
-                            setValue({ ...values, message: event.target.value })
+                            setFormValues({
+                                ...formValues,
+                                message: event.target.value,
+                            })
                         }
                     ></TextField>
                 </div>
                 <div>
-                    <Button variant="contained" type="submit">
-                        Submit
+                    <Button
+                        disabled={submitting}
+                        variant="contained"
+                        type="submit"
+                    >
+                        {submitting ? 'Submitting' : 'Submit'}
                     </Button>
                 </div>
-                {/* <p>name: {values.name}</p>
-                <p>email: {values.email}</p>
-                <p>message: {values.message}</p> */}
+                {message && (
+                    <Snackbar
+                        open={open}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'center',
+                        }}
+                        autoHideDuration={6000}
+                        onClose={handleClose}
+                    >
+                        <Alert
+                            onClose={handleClose}
+                            severity={message ? 'success' : 'error'}
+                            sx={{ width: '100%' }}
+                        >
+                            {message}
+                        </Alert>
+                    </Snackbar>
+                )}
             </form>
         </div>
     );
