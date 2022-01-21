870f38008c922890a12356d2ad7624ccc3cd5505
Olivia Hyland <1oliviahyland@gmail.com>
2022-01-21 21:42:58 +0000

    Create Navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,53 @@
-import { Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+    AppBar,
+    Drawer,
+    ListItem,
+    List,
+    IconButton,
+    MenuItem,
+    Toolbar,
+    Typography,
+} from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 import {
     Person as PersonIcon,
     Apps as AppsIcon,
     MailOutlineOutlined as MailOutlineOutlinedIcon,
+    Menu as MenuIcon,
 } from '@mui/icons-material';
 import { makeStyles } from '@mui/styles';
 
 const useStyles = makeStyles({
+    drawer: {
+        color: 'white',
+        textTransform: 'uppercase',
+    },
     appBar: {
+        '& .MuiTypography-body1': {
+            borderBottom: '3px solid transparent',
+            color: 'white',
+        },
+        '& .MuiTypography-root': {
+            fontWeight: 'bold',
+            margin: '0 10px',
+            color: 'white',
+        },
+        '& .MuiListItem-root': {
+            color: 'white',
+        },
         '& .MuiToolbar-root': {
             textTransform: 'uppercase',
             display: 'flex',
             justifyContent: 'flex-end',
-            '& .MuiTypography-root': {
-                fontWeight: 'bold',
-                margin: '0 10px',
+        },
+        '& .active': {
+            '& .MuiTypography-body1': {
+                transitionDuration: '0.3s',
+                borderBottom: '3px solid #da57f3',
+                fontWeight: '800',
             },
         },
         '& a': {
@@ -27,30 +59,85 @@ const useStyles = makeStyles({
 
 const Nav = () => {
     const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    const naviagtionArray = [
+        {
+            text: 'Home',
+            url: '/',
+            icon: <PersonIcon />,
+        },
+        {
+            text: 'Portfolio',
+            url: '/portfolio',
+            icon: <AppsIcon />,
+        },
+        {
+            text: 'Contact',
+            url: '/contact',
+            icon: <MailOutlineOutlinedIcon />,
+        },
+    ];
+
     return (
         <AppBar className={classes.appBar}>
             <Toolbar>
-                {[
-                    {
-                        text: 'About',
-                        url: '/',
-                        icon: <PersonIcon />,
-                    },
-                    {
-                        text: 'Portfolio',
-                        url: '/portfolio',
-                        icon: <AppsIcon />,
-                    },
-                    {
-                        text: 'Contact',
-                        url: '/contact',
-                        icon: <MailOutlineOutlinedIcon />,
-                    },
-                ].map((menuItem, index) => (
-                    <Link to={menuItem.url} key={index}>
-                        <Typography>{menuItem.text}</Typography>
-                    </Link>
-                ))}
+                {isMobile ? (
+                    <>
+                        <Drawer
+                            anchor="top"
+                            id="basic-menu"
+                            open={open}
+                            anchorEl={anchorEl}
+                            onClose={handleClose}
+                            MenuListProps={{
+                                'aria-labelledby': 'basic-button',
+                            }}
+                        >
+                            <List>
+                                {naviagtionArray.map((menuItem, index) => (
+                                    <ListItem
+                                        className={classes.drawer}
+                                        onClick={handleClose}
+                                        key={index}
+                                        variant="contained"
+                                        component={NavLink}
+                                        to={menuItem.url}
+                                    >
+                                        {menuItem.text}
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Drawer>
+
+                        <IconButton color="secondary" onClick={handleClick}>
+                            <MenuIcon />
+                        </IconButton>
+                    </>
+                ) : (
+                    <>
+                        {naviagtionArray.map((menuItem, index) => (
+                            <MenuItem
+                                onClick={handleClose}
+                                key={index}
+                                variant="contained"
+                                component={NavLink}
+                                to={menuItem.url}
+                                activeClassName="active"
+                            >
+                                <Typography>{menuItem.text}</Typography>
+                            </MenuItem>
+                        ))}
+                    </>
+                )}
             </Toolbar>
         </AppBar>
     );
