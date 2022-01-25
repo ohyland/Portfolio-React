9609f42b2b3e7339119618b4af9f67ce967fe3fd
Olivia Hyland <1oliviahyland@gmail.com>
2022-01-25 21:30:11 +0000

    update layout on contact page for desktop

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,45 @@
 import React, { useState } from 'react';
-import { Button, TextField, Typography, Alert, Snackbar } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 import axios from 'axios';
+import { makeStyles } from '@mui/styles';
+import {
+    Button,
+    TextField,
+    Typography,
+    Alert,
+    Snackbar,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+} from '@mui/material';
+import {
+    Email as EmailIcon,
+    GitHub as GitHubIcon,
+    LinkedIn as LinkedInIcon,
+} from '@mui/icons-material';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+    contactPage: {
+        [theme.breakpoints.up('sm')]: {
+            display: 'flex',
+        },
+
+        '& .contactDetails': {
+            [theme.breakpoints.up('sm')]: {
+                width: '50%',
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+            },
+        },
+    },
     form: {
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: '50%',
+        },
+
         '& input': {
             color: 'white',
             fontStyle: 'italic',
@@ -14,7 +49,7 @@ const useStyles = makeStyles({
             backgroundColor: '#1a1a1a',
         },
     },
-});
+}));
 
 const Contact = () => {
     const formId = 'PFUsPpdF';
@@ -56,79 +91,103 @@ const Contact = () => {
     };
 
     return (
-        <div>
+        <>
             <Typography variant="h4">Contact Me</Typography>
-            <form className={classes.form} onSubmit={onSubmitForm}>
-                <div>
-                    <TextField
-                        fullWidth
-                        placeholder="Name"
-                        required
-                        onChange={(event) =>
-                            setFormValues({
-                                ...formValues,
-                                name: event.target.value,
-                            })
-                        }
-                    ></TextField>
-                </div>
-                <div>
-                    <TextField
-                        fullWidth
-                        type="email"
-                        placeholder="Email"
-                        required
-                        onChange={(event) =>
-                            setFormValues({
-                                ...formValues,
-                                email: event.target.value,
-                            })
-                        }
-                    ></TextField>
-                </div>
-                <div>
-                    <TextField
-                        fullWidth
-                        required
-                        placeholder="Type your message here..."
-                        onChange={(event) =>
-                            setFormValues({
-                                ...formValues,
-                                message: event.target.value,
-                            })
-                        }
-                    ></TextField>
-                </div>
-                <div>
-                    <Button
-                        disabled={submitting}
-                        variant="contained"
-                        type="submit"
-                    >
-                        {submitting ? 'Submitting' : 'Submit'}
-                    </Button>
-                </div>
-                {message && (
-                    <Snackbar
-                        open={open}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'center',
-                        }}
-                        autoHideDuration={6000}
-                        onClose={handleClose}
-                    >
-                        <Alert
-                            onClose={handleClose}
-                            severity={message ? 'success' : 'error'}
-                            sx={{ width: '100%' }}
+            <div className={classes.contactPage}>
+                <form className={classes.form} onSubmit={onSubmitForm}>
+                    <div>
+                        <TextField
+                            fullWidth
+                            placeholder="Name"
+                            required
+                            onChange={(event) =>
+                                setFormValues({
+                                    ...formValues,
+                                    name: event.target.value,
+                                })
+                            }
+                        ></TextField>
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            type="email"
+                            placeholder="Email"
+                            required
+                            onChange={(event) =>
+                                setFormValues({
+                                    ...formValues,
+                                    email: event.target.value,
+                                })
+                            }
+                        ></TextField>
+                    </div>
+                    <div>
+                        <TextField
+                            fullWidth
+                            required
+                            placeholder="Type your message here..."
+                            onChange={(event) =>
+                                setFormValues({
+                                    ...formValues,
+                                    message: event.target.value,
+                                })
+                            }
+                        ></TextField>
+                    </div>
+                    <div>
+                        <Button
+                            disabled={submitting}
+                            variant="contained"
+                            type="submit"
                         >
-                            {message}
-                        </Alert>
-                    </Snackbar>
-                )}
-            </form>
-        </div>
+                            {submitting ? 'Submitting' : 'Submit'}
+                        </Button>
+                    </div>
+                    {message && (
+                        <Snackbar
+                            open={open}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                            }}
+                            autoHideDuration={6000}
+                            onClose={handleClose}
+                        >
+                            <Alert
+                                onClose={handleClose}
+                                severity={message ? 'success' : 'error'}
+                                sx={{ width: '100%' }}
+                            >
+                                {message}
+                            </Alert>
+                        </Snackbar>
+                    )}
+                </form>
+                <div className="contactDetails">
+                    <List>
+                        <ListItem>
+                            <ListItemIcon>
+                                <LinkedInIcon />
+                            </ListItemIcon>
+                            <ListItemText>{'LinkedIn'}</ListItemText>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <GitHubIcon />
+                            </ListItemIcon>
+                            <ListItemText>{'Github'}</ListItemText>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemIcon>
+                                <EmailIcon />
+                            </ListItemIcon>
+                            <ListItemText>{'Email'}</ListItemText>
+                        </ListItem>
+                    </List>
+                </div>
+            </div>
+        </>
     );
 };
 
